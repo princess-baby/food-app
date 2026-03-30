01c3d8e2585f94093baca5cfe8a572d2de6360df
Princess <princessadelekan2010@gmail.com>
2026-03-30 02:30:22 -0700

    Initial commit create Navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar"
+
+function App() {
+  
+
+  return (
+    // <>
+    // <h1 className="text-3xl text-[red]">Welcome to  new hoizon, i love coding</h1>
+    // </>
+    <Navbar />
+  )
+}
+
+export default App
